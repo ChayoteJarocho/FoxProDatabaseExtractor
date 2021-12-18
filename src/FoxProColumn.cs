b11321d107e20979da4fac50a8adb749f0de64a5
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2021-12-18 13:21:09 -0600

    First app version for .NET Framework 4.8
    - I wrote this back in 2016.
    - Builds successfully.
    - Fully commented code.
    - Pending testing with a real database. Current computer does not have the required VisualFoxPro DLL that allows creating an OleDbConnection to the database file.
    - Pending testing the DataType enums. The original enum had integer values but I could not find a source to explain them, so I changed them to the documented char value.

---
+++ src/FoxProColumn.cs
@@ -0,0 +1,37 @@
+﻿namespace FoxProDatabaseExtractor
+{
+    /// <summary>
+    /// Represents a column from a FoxPro table.
+    /// </summary>
+    public class FoxProColumn
+    {
+        /// <summary>
+        /// The column name.
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The column data type.
+        /// </summary>
+        public DataType DataType
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="FoxProColumn"/> instance.
+        /// </summary>
+        /// <param name="name">The name of the column.</param>
+        /// <param name="dataType">The data type of the column.</param>
+        public FoxProColumn(string name, DataType dataType)
+        {
+            this.Name = name;
+            this.DataType = dataType;
+        }
+    }
+}
